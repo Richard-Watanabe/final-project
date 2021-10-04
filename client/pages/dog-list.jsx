53c2867218e74af18d9cda3309f0e24c33ebbb77
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-10-04 03:48:32 +0000

    Added skeleton of doggo-list page

--- client/pages/dog-list.jsx
+++ client/pages/dog-list.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppContext from '../lib/app-context';
+import { Redirect, Link } from 'react-router-dom';
 
 export default class DogList extends React.Component {
   constructor(props) {
@@ -10,7 +11,20 @@ export default class DogList extends React.Component {
   }
 
   render() {
-    return <h1></h1>;
+    const { user } = this.context;
+    if (!user) return <Redirect to="/" />;
+    return (
+      <div className="d-flex justify-content-center align-items-center full-screen">
+        <div className="inner-white">
+          <Link to="/home" className="go-back d-inline-block">&lt; Back to logs</Link>
+            <div className="text-center name-div add-dog-contain">
+              <div className="d-flex add-name-header text-nowrap">
+                <h2>My Doggo List</h2>
+              </div>
+            </div>
+        </div>
+      </div>
+    );
   }
 }
 
